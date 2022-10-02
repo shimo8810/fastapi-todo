5506e2b339eaf5d105752b3d2a3234bfe6e45607
shimo8810 <shimo.8810@gmail.com>
2022-10-02 18:47:56 +0900

    [update] router の設定を追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from api.routers import done, task
+
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+app.include_router(task.router)
+app.include_router(done.router)
